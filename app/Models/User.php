5180f2f416e58799b868b54dce7a10e74347bfaa
Precious Julia Kionisala <122810175+pjcodingx@users.noreply.github.com>
2026-03-06 23:53:43 +0800

    Implement password hashing in User model
    Add setPasswordAttribute method to hash passwords

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -36,6 +37,11 @@ class User extends Authenticatable
         'is_active',
         'profile_picture',
     ];
+
+    public function setPasswordAttribute($value)
+{
+    $this->attributes['password'] = Hash::make($value);
+}
 
     /**
      * The attributes that should be hidden for serialization.
